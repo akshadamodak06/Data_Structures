846690511bdff89f6155a28abe170853eb61da17
akshadamodak06 <161637747+akshadamodak06@users.noreply.github.com>
2024-05-12 23:20:27 +0530

    Update Double_LinkList.cpp

--- Double_LinkList.cpp
+++ Double_LinkList.cpp
@@ -1,82 +1,218 @@
-#include <iostream>
-using namespace std;
-struct node
-{
-    int data;
-    struct node*next;
-    struct node*prev;
-}*s,*s1;
+//Time Complexity for this code :-
+//Insertion and Deletions at front end and back end are O(1).
+//Display forward and Display backward have O(N) time complexity.
+// Search function has worst case time complexity of O(N) and best case time complexity of O(1).
 
-class DoubleLinkedList
+#include<iostream>
+using namespace std;
+class node 
+{
+    public:
+    int data;
+    node* left;
+    node *right;
+    node(int val)
+    {
+        data=val;
+        left=right=NULL;
+    }
+};
+
+class DLinkList
 {
     private:
-    struct node *tmp,*fnode,*gnode;
-    int num;
+    node *head,*tail;
     public:
-    DoubleLinkedList(int n)
+    DLinkList()
     {
-        cout<<"Enter element 1 : ";
-        cin>>num;
-        s=(struct node*)malloc(sizeof (struct node));
-        if(s==NULL)
+        head=tail=NULL;
+    }
+    void insert_front(int val)
+    {
+        node *tmp=new node(val);
+        if(head==NULL)
         {
-            cout<<"Memory not allocated";
+            head=tail=tmp;
         }
         else
         {
-            s->data=num;
-            s->next=NULL;
-            s->prev=NULL;
-            tmp=s;
-            for(int i=2;i<=n;i++)
+            tmp->right=head;
+            head->left=tmp;
+            head=tmp;
+        }
+    }
+
+    void insert_back(int val)
+    {
+        node *tmp=new node(val);
+        if(head==NULL)
+        {
+            head=tail=tmp;
+        }
+        else
+        {
+            tmp->left=tail;
+            tail->right=tmp;
+            tail=tmp;
+        }
+    }
+
+    void delete_front()
+    {
+        if(head==NULL)
+        {
+            cout<<"List is empty."<<endl;
+        }
+        else if (head == tail)
+        {
+            delete head;
+            delete tail;
+            head=NULL;
+            tail=NULL;
+        }
+        else
+        {
+            head=head->right;  
+            head->left=NULL;          
+        }
+    }
+
+     void delete_back()
+    {
+        if(head==NULL)
+        {
+            cout<<"List is empty."<<endl;
+        }
+        else if (head == tail)
+        {
+            delete head;
+            delete tail;
+            head=NULL;
+            tail=NULL;
+        }
+        else
+        {
+            tail=tail->left; 
+            tail->right=NULL;           
+        }
+    }
+
+    void display_f()
+    {
+        node *tmp=head;
+        cout<<"Double LinkList Forward : ";
+        while(tmp->right != NULL)
+        {
+            cout<<tmp->data<<" ";
+            tmp=tmp->right;
+        }
+        cout<<tmp->data<<" "; 
+    }
+
+    void display_b()
+    {
+        node *tmp=tail;
+        cout<<"Double LinkList Backward : ";
+        while(tmp->left != NULL)
+        {
+            cout<<tmp->data<<" ";
+            tmp=tmp->left;
+        }
+        cout<<tmp->data<<" "; 
+    }
+
+    void search(int val)
+    {
+        node *tmp=head;
+        while(tmp->right != NULL)
+        {
+            if(tmp->data==val)
             {
-                cout<<"Enter element "<<i<<" : ";
-                cin>>num;
-                fnode=(struct node *)malloc(sizeof(struct node));
-                if(fnode==NULL)
-                {
-                    cout<<"Memory not allocated.";
-                }
-                else
-                {
-                    
-                    fnode->data=num;
-                    fnode->prev=tmp;
-                    fnode->next=NULL;
-                    tmp->next=fnode;
-                    tmp=tmp->next;
-                }
+                cout<<"Value "<<val<<" exists in the Circular Linked List .";
+                return;
             }
+            tmp=tmp->right;
         }
-        s1=tmp;
-    }
-
-    void DisplayF()
-    {
-        tmp=s;
-        while(tmp!=NULL)
+        if(tmp->data==val)
         {
-            cout<<"Element is : "<<tmp->data<<endl;
-            tmp=tmp->next;
-
+            cout<<"Value "<<val<<" exists in the Circular Linked List .";
         }
-    }
-
-    void DisplayB()
-    {
-        tmp=s1;
-        while(tmp->prev !=NULL)
+        else
         {
-            cout<<"Element is : "<<tmp->data<<endl;
-            tmp=tmp->prev;
+            cout<<"Value "<<val<<" does not exist in the Circular Linked List .";
         }
-        cout<<"Element is : "<<tmp->data<<endl;
     }
+
 };
 
 int main()
 {
-    DoubleLinkedList dll(5);
-    dll.DisplayF();
-    dll.DisplayB();
+    DLinkList d;
+    int num, num1, n;
+    label:
+    cout << "Enter 0 to insert\nEnter 1 to delete\nEnter 2 to display list forward \nEnter 3 to display list backward \nEnter 4 to search\nEnter 5 to exit\n\n";
+    cin >> num;
+    if (num == 0)
+    {
+        cout << "Enter the value to be inserted : ";
+        cin >> n;
+        cout << "Enter 0 to insert at front\nEnter 1 to insert at back\n\n";
+        cin >> num1;
+        if (num1 == 0)
+        {
+            d.insert_front(n);
+        }
+        else
+        {
+            d.insert_back(n);
+        }
+        cout << endl;
+        goto label;
+    }
+    else if (num == 1)
+    {
+        cout << "Enter 0 to delete at front\nEnter 1 to delete at back ";
+        cin >> num1;
+        if (num1 == 0)
+        {
+            d.delete_front();
+        }
+        else
+        {
+            d.delete_back();
+        }
+        cout << endl;
+        goto label;
+    }
+    else if (num == 2)
+    {
+        d.display_f();
+        cout << endl;
+        goto label;
+    }
+    else if (num == 3)
+    {
+        d.display_b();
+        cout << endl;
+        goto label;
+    }
+    else if (num==4)
+    {
+        cout<<"Enter the number you want to search : ";
+        cin>>num1;
+        d.search(num1);
+        cout<<endl<<endl;
+        goto label;
+    }
+    else  if(num == 5)
+    {
+        cout << "------------------------------Thank You . Program Ends Here. ------------------------------------------";
+        return 0;
+    }
+    else
+    {
+        cout<<"Enter valid number";
+        cout<<endl<<endl;
+        goto label;
+    }
 }
